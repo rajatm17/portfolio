e06ae5c70291cfac1a45da558d495ca948bcc09f
rajatm17 <93270086+rajatm17@users.noreply.github.com>
2024-04-17 16:38:55 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,12 +14,12 @@ const config: Config = {
       keyframes: {
         spotlight: {
           '0%': {
-            opacity: '0',
-            transform: 'translate(-72%, -62%) scale(0.5)',
+            'opacity': '0',
+            'transform': 'translate(-72%, -62%) scale(0.5)',
           },
           '100%': {
-            opacity: '1',
-            transform: 'translate(-50%,-40%) scale(1)',
+            'opacity': '1',
+            'transform': 'translate(-50%,-40%) scale(1)',
           },
         },
       },
